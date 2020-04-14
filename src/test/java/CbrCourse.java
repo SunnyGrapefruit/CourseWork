3818263bbc326d4166dac06097b2283b6938b2cf
SunnyGrapefruit <wir_die_traumer@mail.ru>
2020-04-14 12:55:07 +0500

    Tinkoff Selenium

--- src/test/java/CbrCourse.java
+++ src/test/java/CbrCourse.java
@@ -3,6 +3,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 import org.junit.Before;
@@ -29,38 +30,55 @@ public class CbrCourse {
 //        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
 
-    //Проверка статуса
-    public Response getWith200Status(String endPoint){
+    public ValidatableResponse getWith200Status(String endPoint){
         requestSpecification = RestAssured.given().contentType(ContentType.JSON);
-        return requestSpecification.get(endPoint);
+        return requestSpecification.get(endPoint).then()
+                // .statusCode(HttpStatus.SC_OK)
+                .assertThat().statusCode(200);
     }
 
-    @Test //Проверка значения
-    public void getCBRCoursesTest(){
-        getWith200Status("https://www.cbr-xml-daily.ru/daily_json.js").then()
-//                .statusCode(HttpStatus.SC_OK)
-                .assertThat().statusCode(200)
+    @Test //Проверка значения статуса
+    public void getWith200StatusTest(){
+        getWith200Status("https://www.cbr-xml-daily.ru/daily_json.js")
                 .body("Valute.USD.ID", equalTo("R01235"))
                 .body("Valute.EUR.ID", equalTo("R01239"));
     }
 
-
-    public Response getName(String endPoint){
+    public ValidatableResponse getCourse(String endPoint){
         requestSpecification = RestAssured.given().contentType(ContentType.JSON);
-        return requestSpecification
-                .get(endPoint);
+        return requestSpecification.get(endPoint).then()
+                .body("Valute.USD.ID", equalTo("R01235"))
+                .body("Valute.EUR.ID", equalTo("R01239"));
     }
 
-    @Test //Проверка значения заголовка
-    public void getNameTest(){
-        getName("https://www.cbr-xml-daily.ru/daily_json.js").then()
+    @Test //Проверка наличия евро и доллара
+    public void getCourseTest(){
+        getCourse("https://www.cbr-xml-daily.ru/daily_json.js");
+    }
+
+    public ValidatableResponse getName(String endPoint){
+        requestSpecification = RestAssured.given().contentType(ContentType.JSON);
+        return requestSpecification.get(endPoint)
+                .then()
                 .header("Content-Type", equalTo("application/javascript; charset=utf-8"));
     }
 
-    public RequestSpecification getSome(String endPoint){
-        RequestSpecification requestSpec = requestSpecification.given()
-                .baseUri("http://cookiemonster.com")
-                .header("Language", "en");
-        return requestSpec;
+    @Test //Проверка заголовка
+    public void getNameTest(){
+        getName("https://www.cbr-xml-daily.ru/daily_json.js");
     }
+
+    public ValidatableResponse getDate(String endPoint){
+        requestSpecification = RestAssured.given().contentType(ContentType.JSON);
+        return requestSpecification.get(endPoint)
+                .then()
+                .body("Timestamp", equalTo("2020-04-13T23:00:00+03:00"))
+                .body("Date", equalTo("2020-04-14T11:30:00+03:00"));
+    }
+
+    @Test //Проверка даты
+    public void getDateTest(){
+        getDate("https://www.cbr-xml-daily.ru/daily_json.js");
+    }
+
 }
